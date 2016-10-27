a15a7316b38e9e9e09c84226940c2e1e39449e1f
Whitney <bdwseybold@gmail.com>
2016-10-27 15:51:31 -0500

    added queries

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require('body-parser')
+
+var app = express();
+
+app.use('/', express.static(__dirname + '/public'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.get('/api/data', function (req, res) {
+  let data = "Data posted";
+  // get data from some source.... api or db
+  console.log("got data");
+  res.send(data);
+});
+
+app.post('/api/data', function (req, res) {
+  let theInfoWePosted = req.body.firstName;
+  res.send(theInfoWePosted);
+});
+
+
+app.listen(3000, function () {
+  console.log('See this website at localhost:3000');
+});
